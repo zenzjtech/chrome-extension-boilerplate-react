fd5e8297af42a29d86ea8b95617147ea46440956
zenji <canhtoannguyen60@gmail.com>
2023-01-22 10:17:13 +0700

    Add material-ui

--- src/pages/Options/Options.tsx
+++ src/pages/Options/Options.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import './Options.css';
+import {Typography} from "@mui/material";
 
 interface Props {
   title: string;
 }
 
 const Options: React.FC<Props> = ({ title }: Props) => {
-  return <div className="OptionsContainer">{title} Page</div>;
+  return (
+      <>
+        <div className="OptionsContainer">{title} Page</div>
+        <Typography
+            variant={"h1"}
+            color={"primary"}
+            className={"OptionsContainer"}
+        >
+          Hello from MaterialUI
+        </Typography>
+      </>
+  )
 };
 
 export default Options;
--- src/pages/Options/index.jsx
+++ src/pages/Options/index.jsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client';
 
 import Options from './Options';
 import './index.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 const container = document.getElementById('app-container');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
